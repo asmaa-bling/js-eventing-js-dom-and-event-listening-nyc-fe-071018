430359a7bf37c9f87d4069373f2a71bd4303feba
Asmaa Abbas <asmaa@flatironschool.com>
2018-08-22 01:51:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const main = document.querySelector("#main");
+const paragraph = document.querySelector("p");
+
+main.addEventListener('click', (event) => {
+  alert("Hey, you clicked me!");
+}); 
+
+paragraph.addEventListener('click', (event) => {
+  paragraph.style.backgroundColor = "yellow";
+  alert("Hey, you clicked the p tag");
+});
